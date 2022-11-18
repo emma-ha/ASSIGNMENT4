67c6857b467f93f89649fe81767a83c72f1c2281
Emma Haque <emmaha299@gmail.com>
2022-11-18 17:25:42 -0500

    debits page

--- src/components/Debits.js
+++ src/components/Debits.js
@@ -5,6 +5,7 @@ The Debits component contains information for Debits page view.
 Note: You need to work on this file for the Assignment.
 ==================================================*/
 import {Link} from 'react-router-dom';
+import AccountBalance from './AccountBalance';
 
 const Debits = (props) => {
   // Create the list of Debit items
@@ -15,18 +16,27 @@ const Debits = (props) => {
       return <li key={debit.id}>{debit.amount} {debit.description} {date}</li>
     }) 
   }
+
+  const handleSubmit = (e) =>
+  {
+    e.preventDefault();
+    props.addDebit({ amount: e.target.amount.value, date: new Date().toISOString(),
+      description: e.target.description.value,});
+  }
   // Render the list of Debit items and a form to input new Debit item
   return (
     <div>
       <h1>Debits</h1>
-
+      <br/>
       {debitsView()}
 
-      <form onSubmit={props.addDebit}>
+      <form onSubmit={e => {handleSubmit(e)} }>
         <input type="text" name="description" />
         <input type="number" name="amount" />
         <button type="submit">Add Debit</button>
       </form>
+      <br/>
+      <AccountBalance accountBalance={props.accountBalance}/>
       <br/>
       <Link to="/">Return to Home</Link>
     </div>
