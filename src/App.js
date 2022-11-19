ad45f9ff6e9d53b7497b0b68e0f3b493627ed65f
Emma Haque <emmaha299@gmail.com>
2022-11-18 23:24:39 -0500

    credits and some extra fixes

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ class App extends Component {
     this.state = {
       accountBalance: 1234567.89,
       debitList: [],
+      creditList: [],
       currentUser: {
         userName: 'Joe Smith',
         memberSince: '11/22/99',
@@ -32,15 +33,15 @@ class App extends Component {
   {
     let totalDebit = 0;
     let totalCredit = 0;
-    let API = 'https://moj-api.herokuapp.com/';
+    let API = 'https://moj-api.herokuapp.com/'; //API link
     try 
     {
       let cAPI = await axios.get(API + 'credits');
-      let dAPI = await axios.get(API + 'debits');
-      this.setState({credits: cAPI.data});
-      this.setState({debits: dAPI.data});
+      let dAPI = await axios.get(API + 'debits'); 
+      this.setState({credits: cAPI.data}); //data into list
+      this.setState({debits: dAPI.data}); //data into list
     }
-    catch (error) 
+    catch (error) //error handling 
     {
       if (error.cAPI) 
       {
@@ -53,30 +54,46 @@ class App extends Component {
         console.log(error.dAPI.status);
       }
     }
-    for (let c of this.state.credits)
+    for (let c of this.state.credits) //adding credits
     { 
-      totalDebit = totalDebit + c.amount;
+      totalCredit = totalCredit + c.amount;
     }
-    for (let d of this.state.debits) 
+    for (let d of this.state.debits) //subtracting debits 
     { 
       totalDebit = totalDebit - d.amount;
     }
-    this.setState({accountBalance:((totalCredit+totalDebit)).toFixed(2)});
+    this.setState({accountBalance:((totalCredit + totalDebit)).toFixed(2)});
   }
 
-  addDebit = (d) => 
+  addDebit = (d) => //updating balance
   {
     let update = {
-      amount: Math.round(d.amount * 100)/100,
+      amount: Number(d.amount).toFixed(2),
       date: d.date,
       description: d.description,
       id: d.id
     }
     let us = this.state.debits;
     us.push(update);
-    this.setState({credit: us});
-    this.setState({accountBalance: (Number(this.state.accountBalance) + Number(d.amount)).toFixed(2)});
+    this.setState({debit: us});
+    this.setState({accountBalance: (Number(this.state.accountBalance) - Number(d.amount)).toFixed(2)});
   }
+
+  addCredit = (c) => //updating balance
+  {
+    let update = {
+      amount: Number(c.amount).toFixed(2),
+      date: c.date,
+      description: c.description,
+      id: c.id
+    }
+    let us = this.state.credits;
+    us.push(update);
+    this.setState({credit: us});
+    this.setState({accountBalance: (Number(this.state.accountBalance) + Number(c.amount)).toFixed(2)});
+  }
+
+
 
   // Update state's currentUser (userName) after "Log In" button is clicked
   mockLogIn = (logInInfo) => {  
@@ -94,7 +111,7 @@ class App extends Component {
     );
     const LogInComponent = () => (<LogIn user={this.state.currentUser} mockLogIn={this.mockLogIn} />)
     const DebitsComponent = () => (<Debits debits={this.state.debits} accountBalance={this.state.accountBalance} addDebit = {this.addDebit}/>) 
-
+    const CreditsComponent = () => (<Credits credits={this.state.credits} accountBalance={this.state.accountBalance} addCredit = {this.addCredit}/>)
     // Important: Include the "basename" in Router, which is needed for deploying the React app to GitHub Pages
     return (
       <Router basename="/ASSIGNMENT4">
@@ -102,7 +119,7 @@ class App extends Component {
           <Route exact path="/" render={HomeComponent}/>
           <Route exact path="/userProfile" render={UserProfileComponent}/>
           <Route exact path="/login" render={LogInComponent}/>
-          <Route exact path="/credits" render={Credits}/>
+          <Route exact path="/credits" render={CreditsComponent}/>
           <Route exact path="/debits" render={DebitsComponent}/>
         </div>
       </Router>
--- src/components/Credits.js
+++ src/components/Credits.js
@@ -5,11 +5,36 @@ The Credits component contains information for Credits page view.
 Note: You need to work on this file for the Assignment.
 ==================================================*/
 import {Link} from 'react-router-dom';
+import AccountBalance from './AccountBalance';
 
 const Credits = (props) => {
+  let creditsView = () => {
+    const { credits } = props;
+    return credits.map((credits) => {  // Extract "id", "amount", "description" and "date" properties of each debits JSON array element
+      let date = credits.date.slice(0,10);
+      return <li key={credits.id}>{credits.amount} {credits.description} {date}</li>
+    }) 
+  }
+
+  const handleSubmit = (e) =>
+  {
+    e.preventDefault();
+    props.addCredit({ amount: e.target.amount.value, date: new Date().toISOString(),
+      description: e.target.description.value,}); //add to list
+  }
   return (
     <div>
-      <h1>Credits</h1>
+      <h1>Credits</h1> 
+
+      {creditsView()}
+
+      <form onSubmit={e => {handleSubmit(e)} }>
+        <input type="text" name="description" />
+        <input type="number" name="amount" step ="0.01"/>
+        <button type="submit">Add Credit</button>
+      </form>
+      <br/>
+      <AccountBalance accountBalance={props.accountBalance}/> 
       <br/>
       <Link to="/">Return to Home</Link>
     </div>
