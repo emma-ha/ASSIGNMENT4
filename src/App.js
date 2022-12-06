873437f85bcdfeff8efd841868a7b8d43cac24ab
Emma Haque <emmaha299@gmail.com>
2022-12-05 21:47:35 -0500

    fixes

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class App extends Component {
   constructor() {  
     super(); 
     this.state = {
-      accountBalance: 1234567.89,
+      accountBalance: 0.00,
       debits: [],
       credits: [],
       currentUser: {
